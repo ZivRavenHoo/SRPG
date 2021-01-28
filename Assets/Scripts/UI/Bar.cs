e0f44890860e9395a730f37817cc8360e4f53eeb
guoxuan <guoxuan@wepie.com>
2021-01-28 16:15:44 +0800

    进度条UI和控制脚本
    可以用来做各种进度条,血条等等

---
+++ Assets/Scripts/UI/Bar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField] private Transform fill;
+
+    private float percent;
+    public float Percent
+    {
+        get => percent;
+        set
+        {
+            if (value > 1)
+                value = 1;
+            else if (value < 0)
+                value = 0;
+
+            if (value == percent)
+                return;
+            percent = value;
+            Refresh();
+        }
+    }
+
+    private void Awake()
+    {
+        Percent = 1f;
+    }
+
+    private void Update()
+    {
+        Percent -= Time.deltaTime/10;
+    }
+
+    private void Refresh()
+    {
+        Vector3 scale = fill.localScale;
+        scale.x = percent;
+        fill.localScale = scale;
+    }
+}
